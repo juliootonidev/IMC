6faa12b638927694051a3fe1742374d16fe5fd10
Julio Gabriel Profeta Otoni <julio.otoni@outlook.com>
2020-12-08 21:29:01 -0300

    Arquivo atualizado

--- Cálculo IMC.c
+++ Cálculo IMC.c
@@ -1,84 +1,99 @@
-#include <stdio.h>
+/*
+ Autor: Julio Gabriel P. Otoni 
+ Data : 08/12/2001
+*/
 
+#include <stdio.h> // Biblioteca. 
+
+// Função para calcular IMC, facilita o usa da equação.
 float CalcularIMC(float peso, float altura){
 	
 	float IMC = peso / (altura*altura);
 	
 	return IMC;
 }
+
+// Código de execução do IMC.
 int main (void){
 	
 	int opcao;
 	float peso, altura;
-
+	
+	printf (" \n***********************************************");
 	printf ("\n Digite o numero que deseja consultar:\n");
-	printf ("\n [1] Calcular IMC\n");
-	printf (" [2] Sobre o prorgama\n");
+	printf ("\n [1] Calcular Indice de Massa Corporal - IMC\n");
+	printf (" [2] Sobre\n");
 	scanf ("%d", &opcao);
+	printf (" \n***********************************************");
 	
 	switch (opcao){
 		case 1:
-			//faça a operação sobre o imc
+			
+			//Calcula o IMC. 
+			
 			printf("\n Descubra seu IMC, digite os dados abaixo:\n");
 			printf(" Digite seu peso: ");
 			scanf ("%f", &peso);
 			printf(" Digite sua altura: ");
 			scanf ("%f", &altura);
 			
-			float IMC = CalcularIMC(peso,altura);
+			float IMC = CalcularIMC(peso,altura); // Uso da função da equação do IMC.
 			
 			if (IMC <= 17){
 			
 				printf("\n Seu IMC: %1.1f\n", IMC); 
-				printf(" Categoteria: muito abaixo do peso");
+				printf(" Categoteria: Muito abaixo do peso");
 			}
 		
 			else if (IMC > 17 && IMC <= 18.49){
 				
 				printf("\n Seu IMC: %1.1f\n", IMC); 
-				printf(" Categoria: abaixo do peso ideal");
+				printf(" Categoria: Abaixo do peso ideal");
 			}
 			else if (IMC >= 18.50 && IMC <= 24.99){
 				
 				printf("\n Seu IMC: %1.1f\n", IMC); 
-				printf(" Categoria: peso considerado normal");
+				printf(" Categoria: Peso considerado normal");
 			}
 			else if (IMC >= 25 && IMC <= 29.99){
 				
 				printf("\n Seu IMC: %1.1f\n",IMC); 
-				printf(" Categoria: acima do peso");
+				printf(" Categoria: Acima do peso");
 			}
 			else if (IMC >= 30 && IMC <= 34.99){
 				
 				printf("\n Seu IMC: %1.1f\n", IMC);
-				printf(" Categoria: obesidade grau I");
+				printf(" Categoria: Obesidade grau I");
 			}
 			else if (IMC >= 35 && IMC <= 39.99){
 				
 				printf("\n Seu IMC: %1.1f\n", IMC);
-				printf(" Categoria: obesidade grau II (severa)");
+				printf(" Categoria: Obesidade grau II (severa)");
 			}
 			else if (IMC >= 40){
 				
-				printf(" Seu IMC: %1.1f\n", IMC);
-				printf(" Categoria: obesidade grau III ( obesidade morbida)");
+				printf("\n Seu IMC: %1.1f\n", IMC);
+				printf(" Categoria: Obesidade grau III (obesidade morbida)");
 			}
 			break;
 		case 2:
-			//obejtivo do programa
+			// Objetivo do programa para o usuário.
+			
 			printf("\n Esse programa tem por obejivo informar o seu IMC\n");
 			printf(" e mostrar a categoria que se encontra.\n");
 			printf("\n SEGUE ABAIXO AS CATEGORIAS EXISTENTES: \n");
-			printf("\n Abaixo de 17: muito abaixo do peso\n");
-			printf("\n Entre 17 e 18,49: abaixo do peso ideal\n");
-			printf("\n Entre 18,5 e 24,99: peso considerado normal\n");
-			printf("\n Entre 25 e 29,99: acima do peso\n");
-			printf("\n Entre 30 e 34,99: obesidade grau I\n");
-			printf("\n Entre 35 e 39,99: obesidade grau II (severa)\n");
-			printf("\n Acima de 40: obesidade grau III (conhecida como obesidade morbida)\n");
+			printf("\n Abaixo de 17: Muito abaixo do peso\n");
+			printf("\n Entre 17 e 18,49: Abaixo do peso ideal\n");
+			printf("\n Entre 18,5 e 24,99: Peso considerado normal\n");
+			printf("\n Entre 25 e 29,99: Acima do peso\n");
+			printf("\n Entre 30 e 34,99: Obesidade grau I\n");
+			printf("\n Entre 35 e 39,99: Obesidade grau II (severa)\n");
+			printf("\n Acima de 40: Obesidade grau III (conhecida como obesidade morbida)\n");
 			break;
 	
 		default:
+			// Caso a informação não seja coerente com  as registradas, mostra essa mensagem de erro.
+			
 			printf("\n Informação invalida");
 			break;
 	}
